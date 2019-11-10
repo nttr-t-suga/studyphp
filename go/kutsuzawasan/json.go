cccd7bb49f2d0faba4f61c3721e49033d296af3f
Takahiro Suga <mustplus3692@outlook.jp>
2019-11-10 21:58:06 +0900

    いえ

---
+++ go/kutsuzawasan/json.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"reflect"
+)
+
+func main() {
+	// type Json struct {
+	// 	Num int `json:"num"`
+	// }
+	values := url.Values{}
+	values.Add("alphabet", "Z")
+	resp, _ := http.PostForm("http://localhost:8080/alpha", values)
+	htmlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	fmt.Println(reflect.TypeOf(resp.Body))
+	fmt.Println(reflect.TypeOf(htmlData))
+
+	// var x = string(htmlData)
+	// fmt.Println(x.name)
+	// var buf bytes.Buffer
+	// enc := json.NewEncoder(resp.Body)
+	// if err := enc.Encode(buf); err != nil {
+	// 	fmt.Println(1111)
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(htmlData)
+	// if resp.StatusCode == 200 { //<--- new
+	// 	fmt.Println("response 200") //<--- new
+	// } else if resp.StatusCode == 406 { //<--- new
+	// 	// num = -num                  //<--- new
+	// 	fmt.Println("response 406") //<--- new
+	// } //<--- newtype Person struct {
+
+	// fmt.Println(reflect.TypeOf(p.Name))
+	// fmt.Println(err)
+
+}
